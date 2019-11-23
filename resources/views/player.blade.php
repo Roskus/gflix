39c70f1e42b51e2774e4b093cd1f5046832a5743
Gustavo Novaro <gnovaro@gmail.com>
2019-11-23 01:18:39 +0100

    Fix path files

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
-<video id="video" controls preload="metadata">
-    <source src="asset/upload/2019/mib/mib.mkv" type="video/mp4">
-    <track label="Español" kind="subtitles" srclang="es" src="asset/upload/2019/mib/mib-es.vtt" default>
-    <track label="English" kind="subtitles" srclang="en" src="captions/vtt/sintel-en.vtt">
-</video>
-
+<div class="container-fluid">
+<div class="row-fluid">
+    <video id="video" controls preload="metadata" class="col">
+        <source src="/asset/upload/2019/mib/mib.mp4" type="video/mp4">
+        <track label="Español" kind="subtitles" srclang="es" src="/asset/upload/2019/mib/mib-es.vtt" default>
+        <track label="English" kind="subtitles" srclang="en" src="captions/vtt/sintel-en.vtt">
+    </video>
+</div>
+</div>
 @endsection
