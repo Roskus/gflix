233ff75b4fd0642d638a358c6a9d3bd6c844dc6b
Gustavo Novaro <gnovaro@gmail.com>
2021-03-01 07:27:50 +0100

    Remove composer.lock from .gitignore

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -4,8 +4,8 @@
 <div class="container-fluid">
 <div class="row-fluid">
     <video id="video" controls preload="metadata" class="col">
-        <source src="/asset/upload/2019/mib/mib.mp4" type="video/mp4">
-        <track label="Español" kind="subtitles" srclang="es" src="/asset/upload/2019/mib/mib-es.vtt" default>
+        <source src="/asset/upload/roskus/roskus-video-modelo-nlp.mp4" type="video/mp4">
+        <track label="Español" kind="subtitles" srclang="es" src="/asset/upload/roskus/roskus-video-modelo-nlp.vtt" default>
         <track label="English" kind="subtitles" srclang="en" src="captions/vtt/sintel-en.vtt">
     </video>
 </div>
