1089624222a06b56e847724c3f09bff03cc5dc8f
Gustavo Novaro <gnovaro@gmail.com>
2024-02-25 22:32:44 +0100

    refactor(admin): Improve Admin config
    Adding logo, fix name

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Open Admin',
+    'name' => 'Gflix Admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Open</b> Admin',
+    'logo' => '<img src="/asset/img/gflix_logo.png" alt="GFlix" height="40"> Admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>OA</b>',
+    'logo-mini' => '<b>GA</b>',
 
     /*
     |--------------------------------------------------------------------------
