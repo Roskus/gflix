24c8f905300af7f909bcb5c22bbd39b60afb4cea
Gustavo Novaro <gnovaro@gmail.com>
2024-05-19 15:01:03 +0200

    fix(slider): Make slider slower

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,10 +101,12 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script>
     const myCarouselElement = document.querySelector('#carousel')
-    const carousel = new bootstrap.Carousel(myCarouselElement, {
-        interval: 600,
-        touch: false
-    })
+    if(carousel) {
+        const carousel = new bootstrap.Carousel(myCarouselElement, {
+            interval: 1200,
+            touch: false
+        });
+    }
 </script>
 </body>
 </html>
