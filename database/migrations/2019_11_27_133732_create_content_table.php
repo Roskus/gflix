3d766f70bb51600314ddb9ceef2edcafa315a730
Shift <shift@laravelshift.com>
2025-02-27 16:45:05 +0000

    Adopt anonymous migrations

--- database/migrations/2019_11_27_133732_create_content_table.php
+++ database/migrations/2019_11_27_133732_create_content_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContentTable extends Migration
+return new class extends Migration
 {
 
     /**
@@ -41,4 +41,4 @@ class CreateContentTable extends Migration
     {
         Schema::dropIfExists('content');
     }
-}
+};
