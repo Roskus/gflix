fe130e687c5a7dac1f968171684c46b63483e0cd
Gustavo Novaro <gnovaro@gmail.com>
2026-06-18 19:21:47 +0200

    refactor(global) Add AdminMenuSeeder to database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            AdminMenuSeeder::class,
             CategorySeeder::class,
         ]);
     }
